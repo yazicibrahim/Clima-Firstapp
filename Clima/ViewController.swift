3b189eb16cc5f6359605eaa606651bfcdb30612e
İBRAHİM YAZICI <ibrhmyazici@icloud.com>
2023-12-25 00:03:22 +0300

    Initial Commit

---
+++ Clima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by İBRAHİM YAZICI on 25.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
